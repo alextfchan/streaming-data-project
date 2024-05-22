5773bd12d04d4bd7967dc09f1c0ce42fd2acb967
Alex Chan <alextfchan96@gmail.com>
2024-05-22 12:05:39 +0100

    function and testing for get_api_key complete

---
+++ test/test_transformation_functions/test_get_api_key.py
@@ -0,0 +1,42 @@
+import os
+import logging
+import json
+import pytest
+import boto3
+from moto import mock_aws
+from transformation_lambda.get_api_key import get_api_key
+
+logger = logging.getLogger()
+
+@pytest.fixture(scope="function")
+def aws_credentials():
+    os.environ["AWS_ACCESS_KEY_ID"] = "testing"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
+    os.environ["AWS_DEFAULT_REGION"] = "eu-west-2"
+
+@mock_aws
+def test_ClientError_secret_that_does_not_exist(caplog):
+    with caplog.at_level(logging.ERROR):
+        get_api_key("fake_secret")
+
+        assert "Secret fake_secret does not exist." in caplog.text
+
+@mock_aws
+def test_KeyError_incorrect_key_in_secret(caplog):
+    secret_name = "fake_secret_name"
+    client = boto3.client("secretsmanager", region_name="eu-west-2")
+    client.create_secret(Name=secret_name, SecretString=json.dumps({"incorrect_key": "test_value"}))
+
+    with caplog.at_level(logging.ERROR):
+        get_api_key("fake_secret_name")
+
+        assert "Incorrect key stored in secrets manager. Secret: fake_secret_name, should have key: 'api_key'." in caplog.text  #noqa: E501
+
+@mock_aws
+def test_connection_params_look_like_expected():
+    secret_string = {"api_key": "mock real key"}
+
+    client = boto3.client("secretsmanager", region_name="eu-west-2")
+    client.create_secret(Name="Mock", SecretString=json.dumps(secret_string))
+
+    assert get_api_key("Mock") == "mock real key"
