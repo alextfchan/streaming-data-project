7dbc1bdda529e2a4fe3b57e0599ecb7539b7cd4e
Alex Chan <alextfchan96@gmail.com>
2024-06-10 05:58:46 +0100

    Util function for transformation lambda: write_file.py + testing complete

---
+++ src/transformation_lambda/write_file.py
@@ -0,0 +1,54 @@
+from datetime import datetime as dt
+import logging
+import boto3
+from botocore.exceptions import ClientError
+
+logging.basicConfig()
+logger = logging.getLogger("transformation_lambda")
+logger.setLevel(logging.INFO)
+
+
+def write_file(content):
+    """
+    Gets the content from the API, and stores it in the s3 bucket.
+
+    Parameters
+    ----------
+    content : dict (required)
+        The content to be stored in the S3 bucket.
+
+    Raises
+    -------
+    ClientError
+        If there is an issue with putting the object into the S3 bucket.
+
+    Returns
+    -------
+    None
+    """
+
+    client = boto3.client("s3")
+    date = dt.now()
+    year = date.year
+    month = date.month
+    day = date.day
+    time = date.strftime("%H%M%S")
+
+    file_name = f"{year}-{month}-{day}-{time}-transformed-content.json"
+
+    try:
+        if content is None:
+            logging.error("No search terms provided.")
+            raise Exception("No search terms provided.")
+
+        response = client.put_object(
+            Body=str(content),
+            Bucket="streaming-data-transformed-data-bucket",
+            Key=file_name
+        )
+
+        if response["ResponseMetadata"]["HTTPStatusCode"] == 200:
+            logger.info("Success. File %s saved.", file_name)
+    except ClientError as e:
+        logger.error(e)
+        raise e
---
+++ test/test_transformation_functions/test_write_file.py
@@ -0,0 +1,72 @@
+import os
+from datetime import datetime as dt
+import logging
+import pytest
+import time_machine
+import boto3
+from moto import mock_aws
+from transformation_lambda.write_file import write_file
+
+logger = logging.getLogger("MyLogger")
+logger.setLevel(logging.INFO)
+
+
+@pytest.fixture(scope="function")
+def aws_credentials():
+    """Mocked AWS Credentials for moto."""
+    os.environ["AWS_ACCESS_KEY_ID"] = "testing"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
+    os.environ["AWS_DEFAULT_REGION"] = "eu-west-2"
+
+@mock_aws
+class TestWriteFile:
+    def test_write_file(self):
+        search_terms = '{"search_term": "machine learning", "date_from": "date_from=2023-01-01", "reference": "Guardian_content"}' # noqa 501
+        s3 = boto3.client("s3")
+        s3.create_bucket(
+            Bucket="streaming-data-transformed-data-bucket",
+            CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
+        )
+        write_file(search_terms)
+        response = s3.list_objects(Bucket="streaming-data-transformed-data-bucket") # noqa 501
+        assert len(response["Contents"]) == 1
+
+    @time_machine.travel(dt(2020, 1, 1, 17, 30, 19))
+    def test_check_correct_file_name_to_bucket(self):
+        search_terms = '{"search_term": "machine learning", "date_from": "date_from=2019-01-01", "reference": "Guardian_content"}' # noqa 501
+        s3 = boto3.client("s3")
+        s3.create_bucket(
+            Bucket="streaming-data-transformed-data-bucket",
+            CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
+        )
+        write_file(search_terms)
+        response = s3.list_objects(Bucket="streaming-data-transformed-data-bucket") # noqa 501
+        assert response["Contents"][0]["Key"] == "2020-1-1-173019-transformed-content.json" # noqa 501
+
+    @time_machine.travel(dt(2020, 1, 1, 17, 30, 19))
+    def test_check_successful_log_output(self, caplog):
+        search_terms = '{"search_term": "machine learning", "date_from": "date_from=2019-01-01", "reference": "Guardian_content"}' # noqa 501
+        s3 = boto3.client("s3")
+        s3.create_bucket(
+            Bucket="streaming-data-transformed-data-bucket",
+            CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
+        )
+        write_file(search_terms)
+        assert (
+             "Success. File 2020-1-1-173019-transformed-content.json saved."
+             in caplog.text
+        )
+
+    def test_raises_client_error_if_empty_json(self, caplog):
+        with caplog.at_level(logging.ERROR):
+            s3 = boto3.client("s3")
+            s3.create_bucket(
+                Bucket="streaming-data-transformed-data-bucket",
+                CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
+            )
+            search_terms = None
+            with pytest.raises(Exception) as message:
+                write_file(search_terms)
+
+            assert "No search terms provided." in str(message.value)
+            assert "No search terms provided." in caplog.text
