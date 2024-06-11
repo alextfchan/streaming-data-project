9f0f766e9a7d9895df9b1b436b782c847b145193
Alex Chan <alextfchan96@gmail.com>
2024-06-11 16:12:56 +0100

    added terraform-set-up folder to allow automating the backend s3 bucket for the main terraform

---
+++ terraform-set-up/main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+    region = "eu-west-2"
+}
+
+resource "aws_s3_bucket" "terraform_remote_state" {
+    bucket = "streaming-data-project-backend-s3-bucket"
+
+    lifecycle {
+        prevent_destroy = true
+    }
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state" {
+    bucket = aws_s3_bucket.terraform_remote_state.id
+
+    versioning_configuration {
+      status = "Enabled"
+    }
+}
