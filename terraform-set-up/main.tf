afa2d3bf0307ec0e89cee8aae5c960e69ec8b9bb
Alex Chan <alextfchan96@gmail.com>
2024-06-13 13:06:09 +0100

    created a new terraform state to assist with automating the pipeline process

--- terraform-set-up/main.tf
+++ terraform-set-up/main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-    region = "eu-west-2"
+    region      = "eu-west-2"
 }
 
 resource "aws_s3_bucket" "terraform_remote_state" {
-    bucket = "streaming-data-project-backend-s3-bucket"
+    bucket      = "streaming-data-project-backend-s3-bucket"
 
     lifecycle {
         prevent_destroy = true
@@ -11,9 +11,9 @@ resource "aws_s3_bucket" "terraform_remote_state" {
 }
 
 resource "aws_s3_bucket_versioning" "terraform_state" {
-    bucket = aws_s3_bucket.terraform_remote_state.id
+    bucket      = aws_s3_bucket.terraform_remote_state.id
 
     versioning_configuration {
-      status = "Enabled"
+      status    = "Enabled"
     }
 }
