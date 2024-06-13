c6db84f82dbcdc1cb249e5ffd995015474e31f2c
Alex Chan <alextfchan96@gmail.com>
2024-06-13 13:07:26 +0100

    script to allow for users to create new searchs in the CLI

---
+++ src/input_tool/create_new_search.py
@@ -0,0 +1,12 @@
+from user_input_tool import user_input_tool
+
+
+def create_new_search():
+    """
+    This function allows the user to input their search terms
+    into the pipeline.
+    """
+    return user_input_tool()
+
+
+create_new_search()
