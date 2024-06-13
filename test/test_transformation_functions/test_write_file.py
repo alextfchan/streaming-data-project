1a9fe368a144cfb19bd375a2e1952aeccc3b37c3
Alex Chan <alextfchan96@gmail.com>
2024-06-13 16:05:03 +0100

    write_file util + testing for transformation handler complete

--- test/test_transformation_functions/test_write_file.py
+++ test/test_transformation_functions/test_write_file.py
@@ -18,6 +18,7 @@ def aws_credentials():
     os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
     os.environ["AWS_DEFAULT_REGION"] = "eu-west-2"
 
+
 @mock_aws
 class TestWriteFile:
     def test_write_file(self):
