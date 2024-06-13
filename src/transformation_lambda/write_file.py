de81b9d6a86d2bd16f257d4e014300878461176d
Alex Chan <alextfchan96@gmail.com>
2024-06-13 16:05:48 +0100

    removed comment

--- src/transformation_lambda/write_file.py
+++ src/transformation_lambda/write_file.py
@@ -44,7 +44,6 @@ def write_file_to_s3(content):
 
         else:
             response = client.put_object(
-                # JSON content must be converted to bytes.
                 Body=json.dumps(content,
                                 indent=4,
                                 ensure_ascii=False
